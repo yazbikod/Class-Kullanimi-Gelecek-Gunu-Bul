e866d79c7b7958f4df57d8032935ec7ca0cc1088
yazbikod <aslan_murathan@hotmail.com>
2022-10-25 01:42:45 +0300

    Proje dosyası ekle.

---
+++ Haftagunu.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_HaftaGunu
+{
+    public class Haftagunu
+    {
+        private static int deger; // değişkene static tanımı uygulandı //
+
+        public Haftagunu()
+        {
+            Console.WriteLine("Uygulamamıza Hoşgeldiniz....");
+
+        }
+        public static void GunBelirle()   // metoda static tanımı uygulandı //
+        {
+            Console.Write("Hesabı haftanın kaçıncı gününden yapmak istiyorsunuz : ");
+            deger = Convert.ToInt16(Console.ReadLine());
+
+            switch (deger)
+            {
+                case 1: Console.WriteLine("Hesap Günü : Pazartesi"); break;
+                case 2: Console.WriteLine("Hesap Günü : Salı"); break;
+                case 3: Console.WriteLine("Hesap Günü : Çarşamba"); break;
+                case 4: Console.WriteLine("Hesap Günü : Perşembe"); break;
+                case 5: Console.WriteLine("Hesap Günü : Cuma"); break;
+                case 6: Console.WriteLine("Hesap Günü : Cumartesi"); break;
+                case 7: Console.WriteLine("Hesap Günü : Pazar"); break;
+                default: Console.WriteLine("Girilen değeri kontrol ediniz!"); break;
+
+            }
+
+        }
+        public static int GetGun()
+        {
+            return deger;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Class_HaftaGunu;
+
+
+Haftagunu.GunBelirle();
+
+Console.Write("Kaç gün eklenecek: ");
+int hesapgunu = Convert.ToInt32(Console.ReadLine());
+
+hesapgunu += Haftagunu.GetGun();
+
+switch (hesapgunu % 7)
+{
+    case 1: Console.WriteLine("Next Day: Pazartesi"); break;
+    case 2: Console.WriteLine("Next Day: Salı"); break;
+    case 3: Console.WriteLine("Next Day: Çarşamba"); break;
+    case 4: Console.WriteLine("Next Day: Perşembe"); break;
+    case 5: Console.WriteLine("Next Day: Cuma"); break;
+    case 6: Console.WriteLine("Next Day: Cumartesi"); break;
+    case 7: Console.WriteLine("Next Day: Pazar"); break;
+    default: Console.WriteLine("Yanlış değer girdiniz!"); break;
+}
